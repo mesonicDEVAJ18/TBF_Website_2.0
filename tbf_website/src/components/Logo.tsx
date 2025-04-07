f2842b4816513d09a930ac417c33cc4cdea14d78
MESONICDevaj <147987753+mesonicDEVAJ18@users.noreply.github.com>
2025-04-07 23:00:29 +0530

    using public and added favicon Fixes : #18

--- tbf_website/src/components/Logo.tsx
+++ tbf_website/src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <img 
-      src="/assets/logo.png" 
+      src="/images/logo.png" 
       alt="Logo" 
       className="w-10 h-10 md:w-12 md:h-12 object-contain"
     />
